527ac45c3a43d5400f97d5ce0fbf2584c8a9e876
Egor Kostan <igorkostan@gmail.com>
2019-10-07 21:34:56 -0700

    Pylint fixes

--- app/core/models.py
+++ app/core/models.py
@@ -1,35 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-	PermissionsMixin
+    PermissionsMixin
 
 # Create your models here.
 
 
 class UserManager(BaseUserManager):
 
-	def create_user(self, email, password=None, **extra_fields):
-		"""
-		Creates and saves a new user
-		:param email:
-		:param password:
-		:param extra_fields:
-		:return user:
-		"""
-		user = self.model(email=email, **extra_fields)
-		user.set_password(password)
-		user.save(using=self._db)
-		return user
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a new user
+        :param email:
+        :param password:
+        :param extra_fields:
+        :return user:
+        """
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-	"""
-	Custom user model that support using email instead of username
-	"""
-	email = models.EmailField(max_length=255, unique=True)
-	name = models.CharField(max_length=255)
-	is_active = models.BooleanField(default=True)
-	is_staff = models.BooleanField(default=False)
+    """
+    Custom user model that support using email instead of username
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-	objects = UserManager()
+    objects = UserManager()
 
-	USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,17 +4,17 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
 
-	def test_create_user_with_email_successful(self):
-		"""
-		Test creating a new user with an email is successful
-		:return:
-		"""
-		email = 'test@test.com'
-		password = 'Test123'
-		user = get_user_model().objects.create_user(
-			email=email,
-			password=password
-		)
+    def test_create_user_with_email_successful(self):
+        """
+        Test creating a new user with an email is successful
+        :return:
+        """
+        email = 'test@test.com'
+        password = 'Test123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
 
-		self.assertEqual(user.email, email)
-		self.assertTrue(user.check_password(password))
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
