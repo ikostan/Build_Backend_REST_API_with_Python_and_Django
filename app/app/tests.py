a1257545a70ee3e2882016df07b56ee462debf8d
Egor Kostan <igorkostan@gmail.com>
2019-10-07 20:47:52 -0700

    Update tests.py

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,6 +4,9 @@ from app.calc import add
 
 class CalcTest(TestCase):
 
-	def test_add_numbers(self):
-		"""test that two numbers are added together"""
-		self.assertEqual(add(3, 8), 11)
+    def test_add_numbers(self):
+        """
+        Test that two numbers are added together
+        :return:
+        """
+        self.assertEqual(add(3, 8), 11)
