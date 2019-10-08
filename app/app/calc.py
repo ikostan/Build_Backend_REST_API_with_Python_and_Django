b81a8942bc63c42e78c9dda216b5f2eeb6b57d22
Egor Kostan <igorkostan@gmail.com>
2019-10-07 20:50:27 -0700

    Update calc.py

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,8 +1,8 @@
 def add(x, y):
-	"""
-	Add two numbers and return the result
+    """
+    Add two numbers and return the result
 	:param x:
 	:param y:
 	:return:
 	"""
-	return x + y
+    return x + y
