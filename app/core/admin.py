3afad634738acb0637655199f33ee7bc203fae4e
Egor Kostan <igorkostan@gmail.com>
2019-10-08 23:29:22 -0700

    add_fieldsets for admin module

--- app/core/admin.py
+++ app/core/admin.py
@@ -24,6 +24,12 @@ class UserAdmin(BaseUserAdmin):
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -48,3 +48,13 @@ class AdminSiteTest(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """
+        Test that the create user page works
+        :return:
+        """
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
