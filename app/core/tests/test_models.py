de621c7b0a2ff2b92a9bfe55edf3b8e4d82f3c11
Egor Kostan <igorkostan@gmail.com>
2019-10-09 14:13:03 -0700

    Update test_models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
-class ModelTests(TestCase):
+class ModelTest(TestCase):
 
     def test_create_user_with_email_successful(self):
         """
