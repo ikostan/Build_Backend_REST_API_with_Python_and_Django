e5f2eac842c873aa904b9970e4cc7628b8309ef2
Egor Kostan <igorkostan@gmail.com>
2019-10-08 23:10:24 -0700

    Admin user model updated

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,13 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
