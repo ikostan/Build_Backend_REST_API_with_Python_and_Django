b59b93daeafabb08d4545da817bd6e3f38f13726
Egor Kostan <igorkostan@gmail.com>
2019-10-15 17:37:02 -0700

    Update test_public_user_api.py

--- app/user/tests/integration/test_public_user_api.py
+++ app/user/tests/integration/test_public_user_api.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
 from django.urls import reverse
-from rest_framework.test import APIClient
 from rest_framework import status
+from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
 
 
 class PublicUserApiTest(TestCase):
@@ -23,7 +23,7 @@ class PublicUserApiTest(TestCase):
     def create_user(**params):
         """
         Create user helper function
-        :param param:
+        :param params:
         :return:
         """
         return get_user_model().objects.create_user(**params)
