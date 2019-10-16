4ad2f8b6c71e9d2d31293716ce11ac99891f807f
Egor Kostan <igorkostan@gmail.com>
2019-10-15 17:40:54 -0700

    Update test_public_user_api.py

--- app/user/tests/integration/test_public_user_api.py
+++ app/user/tests/integration/test_public_user_api.py
@@ -13,6 +13,14 @@ class PublicUserApiTest(TestCase):
     @classmethod
     def setUpClass(cls):
 
+        # The reverse() function can reverse a large
+        # variety of regular expression patterns for URLs,
+        # but not every possible one. The main restriction
+        # at the moment is that the pattern cannot contain
+        # alternative choices using the vertical bar ("|") 
+        # character. You can quite happily use such patterns
+        # for matching against incoming URLs and sending them
+        # off to views, but you cannot reverse such patterns.
         cls.CREATE_USER_URL = reverse('user:create')
 
     def setUp(self) -> None:
