75e9b72f796a71b4689317f4ec45a3cd1c614340
Unknown <unknown@example.com>
2020-08-04 13:34:46 -0700

    Reset Django URLs

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
-    path('api/user/', include('user.urls')),
 ]
